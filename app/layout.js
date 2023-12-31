ee53f70523f3a06655d04ca9ffecce17ef02f4c4
JosiahSco <jscott70@uco.edu>
2023-12-31 00:11:51 -0600

    chore: changing site description

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Asteroid Inc.',
-  description: 'Forgot to change this',
+  description: 'Asteroid Mining Clicker Game',
 }
 
 export default function RootLayout({ children }) {
