c7774fcab8027aca41451e584bab3406730f8260
JosiahSco <jscott70@uco.edu>
2024-01-02 22:57:48 -0600

    feat: adding another couple achievements

--- app/acheivements.js
+++ app/acheivements.js
@@ -34,5 +34,17 @@ export const achievementList = [
         name: 'Now What?',
         description: "You bought everything",
         condition: (state) => state.purchasedUpgrades.includes(6),
-    }
+    },
+    {
+        id: 6,
+        name: 'A Fresh Start',
+        description: "You prestiged!",
+        condition: (state) => state.prestige > 0,
+    },
+    {
+        id: 7,
+        name: 'What are the odds?',
+        description: "1 in a Million chance around every second to get this.",
+        condition: (state) => Math.random() <= (1 / 1_000_000),
+    },
 ]
